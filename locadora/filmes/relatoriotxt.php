1e04312be2c07ddf765355932589c57ade0f0da9
Marcos Jose <marcos.com.j.s@gmail.com>
2015-01-14 00:18:10 -0300

    report txt

---
+++ locadora/filmes/relatoriotxt.php
@@ -0,0 +1,49 @@
+<?php
+    // Chamando a conexão
+    require '../conexao.php';
+    mysqli_query($conexao, "SET NAMES 'UTF8';");
+	
+	// Função para converter strings
+	function converte ($string) {
+		return iconv("UTF-8", "ISO-8859-1", $string);
+	}
+	
+    // Cabeçalho
+    $arquivo = fopen('relatorios/rel_filme.txt', 'w');
+	$texto = str_repeat('-', 122);
+	$texto.= "\r\n";
+	$texto.= str_pad('Cod_Filme', 10, " ", STR_PAD_BOTH).' | ';
+	$texto.= str_pad(converte('Título')	, 34, " ", STR_PAD_RIGHT).' | ';
+	$texto.= str_pad('Sinopse', 46, " ", STR_PAD_RIGHT).' | ';
+	$texto.= str_pad('Quantidade', 10, " ", STR_PAD_RIGHT).' | ';
+	$texto.= str_pad('Categoria', 10, " ", STR_PAD_BOTH);
+	$texto.= "\r\n";
+	$texto.= str_repeat('-', 122);
+	$texto.= "\r\n";
+	fwrite($arquivo, $texto);
+	
+	// Consultando o banco de dados
+	$sql = "SELECT * FROM filme";
+	$resultado = mysqli_query($conexao, $sql);
+	// Escrevendo as linhas com os dados da tabela
+	while($res = mysqli_fetch_array($resultado)) {
+		$res['titulo'] = substr(converte($res['titulo']),0,34);	
+		$res['sinopse'] = substr(converte($res['sinopse']),0,43);
+		
+		// Dados do relatorio
+		$texto = str_pad($res['cod_filme'], 10, ' ', STR_PAD_BOTH).' | ';
+		$texto.= str_pad($res['titulo'], 34, ' ', STR_PAD_RIGHT).' | ';
+		$texto.= str_pad($res['sinopse']."...", 46, ' ', STR_PAD_RIGHT).' | ';
+		$texto.= str_pad($res['quantidade'], 10, ' ', STR_PAD_LEFT).' | ';
+		$texto.= str_pad(converte($res['categoria']), 10, ' ', STR_PAD_BOTH);
+		$texto.= "\r\n";
+		//$texto.= str_repeat('-', 122);
+		//$texto.= "\r\n";
+		fwrite($arquivo, $texto);
+	}
+	fclose($arquivo);
+	echo '<center>';
+    echo "Arquivo gerado com sucesso<br>";
+    echo "<a target=newwindow href='relatorios/rel_filme.txt'> Clique aqui para baixar</a>";
+    echo '</center>';
+?>
